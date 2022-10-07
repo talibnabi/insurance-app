fc9b05c79d967785ce3b23a3d214d8f067d19234
talibnabi <talibnabiyev@gmail.com>
2022-10-07 09:43:10 -0700

    commit

--- insurance-app/src/main/java/com/company/insuranceapp/controller/RoleController.java
+++ insurance-app/src/main/java/com/company/insuranceapp/controller/RoleController.java
@@ -1,16 +1,26 @@
 package com.company.insuranceapp.controller;
 
 import com.company.insuranceapp.model.dto.RoleDTO;
+import com.company.insuranceapp.model.request.RoleRequest;
 import com.company.insuranceapp.service.abstracts.RoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/roles")
 public class RoleController {
     private final RoleService roleService;
-   
+
+    @PostMapping
+    public ResponseEntity<RoleDTO> createRole(@RequestBody @Valid RoleRequest request) {
+        return new ResponseEntity<>(roleService.createRole(request), HttpStatus.CREATED);
+    }
 }
--- insurance-app/src/main/java/com/company/insuranceapp/controller/UserController.java
+++ insurance-app/src/main/java/com/company/insuranceapp/controller/UserController.java
@@ -1,4 +1,34 @@
 package com.company.insuranceapp.controller;
 
+import com.company.insuranceapp.model.dto.UserDTO;
+import com.company.insuranceapp.model.request.RegisterRequest;
+import com.company.insuranceapp.service.abstracts.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/users")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getByUserId(@PathVariable @Min(1) Long userId) {
+        return new ResponseEntity<>(userService.getByUserId(userId), HttpStatus.OK);
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<UserDTO> signUp(@RequestBody @Valid RegisterRequest request) {
+        return new ResponseEntity<>(userService.register(request), CREATED);
+    }
 }
