2797919544df387baed8419ff3ebfe283947ded0
talibnabi <talibnabiyev@gmail.com>
2022-10-07 09:39:20 -0700

    commit

--- insurance-app/src/main/java/com/company/insuranceapp/controller/RoleController.java
+++ insurance-app/src/main/java/com/company/insuranceapp/controller/RoleController.java
@@ -1,4 +1,16 @@
 package com.company.insuranceapp.controller;
 
+import com.company.insuranceapp.model.dto.RoleDTO;
+import com.company.insuranceapp.service.abstracts.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/roles")
 public class RoleController {
+    private final RoleService roleService;
+   
 }
